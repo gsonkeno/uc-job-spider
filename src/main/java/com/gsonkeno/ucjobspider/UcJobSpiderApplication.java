d4a417d465ce31153aad5e937be9305f525febcd
gaosong <805940564@qq.com>
2018-07-05 00:04:57 +0800

    Initial commit

---
+++ src/main/java/com/gsonkeno/ucjobspider/UcJobSpiderApplication.java
@@ -0,0 +1,12 @@
+package com.gsonkeno.ucjobspider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcJobSpiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcJobSpiderApplication.class, args);
+    }
+}
